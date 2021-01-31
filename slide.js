22eecd340ea41e88e49af9aa676fa71324b593f0
yejun0705 <ohman0705@naver.com>
2021-02-01 03:27:43 +0900

    02.01

---
+++ slide.js
@@ -0,0 +1,28 @@
+function plusSlides(n) {
+    showSlides2(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+    let i;
+    let slides2 = document.getElementsByClassName("mySlides2");
+    let dots2 = document.getElementsByClassName("dot-2");
+
+    if (n > slides2.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides2.length
+    }
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
+    }
+    for (i = 0; i < dots2.length; i++) {
+        dots2[i].className = dots2[i].className.replace(" active", "");
+    }
+    slides2[slideIndex - 1].style.display = "block";
+    dots2[slideIndex - 1].className += " active";
+}
