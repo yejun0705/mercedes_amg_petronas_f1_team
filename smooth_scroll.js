d920b7c1a165201030633436f25af8e3e850709a
yejun0705 <ohman0705@naver.com>
2021-02-03 17:08:46 +0900

    smooth_scroll

---
+++ smooth_scroll.js
@@ -0,0 +1,28 @@
+function wheel(event) {
+  let delta = 0;
+  if (event.wheelDelta) {
+    (delta = event.wheelDelta / 120);
+  } else if (event.detail) {
+    (delta = -event.detail / 3);
+  }
+
+  handle(delta);
+  if (event.preventDefault) {
+    (event.preventDefault());
+  }
+  event.returnValue = false;
+}
+
+function handle(delta) {
+  let time = 1000;
+  let distance = 300;
+
+  $('html, body').stop().animate({
+    scrollTop: $(window).scrollTop() - (distance * delta)
+  }, time);
+}
+
+if (window.addEventListener) {
+  window.addEventListener('DOMMouseScroll', wheel, false);
+}
+window.onmousewheel = document.onmousewheel = wheel;
