0441a2094afe85342dc0802a6f0418fd00e7af3b
yejun0705 <ohman0705@naver.com>
2021-02-13 01:46:16 +0900

    section_10

---
+++ d-days.js
@@ -0,0 +1,12 @@
+let dday = new Date("March 26, 2021, 0:00:00").getTime();
+
+setInterval(function () {
+    let today = new Date().getTime();
+    let gap = dday - today;
+    let day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+    let hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+    let sec = Math.ceil((gap % (1000 * 60)) / 1000);
+
+    document.getElementById("count").innerHTML = day + "d " + hour + "h " + min + "m " + sec + "s";
+}, 1000);
